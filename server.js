5df13d0e62a0468549ccbd23ff91b870ca007b37
J.T <jiahui.tan@alumni.ubc.ca>
2021-07-30 16:31:22 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Cross-Origin-Opener-Policy', 'same-origin');
+    res.header('Cross-Origin-Embedder-Policy', 'require-corp');
+    res.header('Cross-Origin-Resource-Policy', 'cross-origin');
+    next();
+});
+
+app.use(express.static('build'));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
+
+app.get('/', function(req, res){
+    res.sendFile('index.html', { root: __dirname});
+});
